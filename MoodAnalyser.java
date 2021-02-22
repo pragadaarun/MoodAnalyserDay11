a3a82bf2bdcf5a7b90b68bd615daa123e510ddf9
pragadaarun <pmaniarunkumar@gmail.com>
2021-02-22 19:52:24 +0530

    First Commit

---
+++ MoodAnalyser.java
@@ -0,0 +1,28 @@
+public class MoodAnalyser {
+    public String message;
+
+    public MoodAnalyser() {
+
+    }
+
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
+        try {
+            if (message.contains("sad")) {
+                return "SAD";
+            } else {
+                return "HAPPY";
+            }
+        } catch (NullPointerException e) {
+            return "HAPPY";
+        }
+    }
+
+
+    public static void main(String Args[]) {
+        System.out.println("Welcome to mood Analyser");
+    }
+}
